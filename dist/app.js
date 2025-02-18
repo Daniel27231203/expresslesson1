a4b821498c36ecae02feb587ecf7209791274de7
Daniel27231203 <daniel27231203@gamil.com>
2025-02-18 13:02:40 +0600

    fix last deploy

--- dist/app.js
+++ dist/app.js
@@ -11,20 +11,15 @@ const routes_1 = __importDefault(require("./routes"));
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const path_1 = __importDefault(require("path"));
 const fs_1 = require("fs");
-// Определяем путь к swagger.json
 const swaggerPath = path_1.default.join(process.cwd(), "src", "swagger.json");
-// Читаем содержимое swagger.json
 const swaggerDocumentation = JSON.parse((0, fs_1.readFileSync)(swaggerPath, "utf-8"));
 const buildServer = () => {
     const server = (0, express_1.default)();
     server.use(express_1.default.json());
-    // Подключаем Swagger UI
     server.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerDocumentation));
-    // Добавляем роут для получения swagger.json
     server.get("/swagger.json", (req, res) => {
         res.status(200).json(swaggerDocumentation);
     });
-    // Основной роут
     server.get("/", (req, res) => {
         res.status(200).send({ message: "Server is Working" });
     });
--- src/app.ts
+++ src/app.ts
@@ -6,29 +6,24 @@ import swaggerUi from "swagger-ui-express";
 import path from "path";
 import { readFileSync } from "fs";
 
-// Определяем путь к swagger.json
 const swaggerPath = path.join(process.cwd(), "src", "swagger.json");
 
-// Читаем содержимое swagger.json
 const swaggerDocumentation = JSON.parse(readFileSync(swaggerPath, "utf-8"));
 
 export const buildServer = () => {
   const server = express();
   server.use(express.json());
 
-  // Подключаем Swagger UI
   server.use(
     "/api-docs",
     swaggerUi.serve,
     swaggerUi.setup(swaggerDocumentation)
   );
 
-  // Добавляем роут для получения swagger.json
   server.get("/swagger.json", (req, res) => {
     res.status(200).json(swaggerDocumentation);
   });
 
-  // Основной роут
   server.get("/", (req, res) => {
     res.status(200).send({ message: "Server is Working" });
   });
